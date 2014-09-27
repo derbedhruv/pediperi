3f4d97fa3d9743c647e31d4508989f7cb1154dd0
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2014-09-27 17:39:43 +0530

    optimized code to reset the LEDs at the start of each function call

--- perimeter_mega/perimeter_mega.ino
+++ perimeter_mega/perimeter_mega.ino
@@ -38,23 +38,23 @@ void lightHerUp(String latitude, String longitude) {
    // for quadrants, the inputs will be ((q), [1,2,3,4]) corresponding to the 4 quadrants possible
    // all LEDs will flash for a particular period of time and then cut off.
    
-   // first check if the latitude value is a number or not..
-   if (isdigit(latitude[0]) == 1) {  // the latitude will only be (2,10) so that's cool, it's only one char
-       // this is the case for a single LED
-       // step 0: turn OFF all latitudes..
+   // we start by resetting all the LEDs
+   // step 1: turn OFF all latitudes..
        for (int h=2; h<=10; h++) {
          digitalWrite(h, LOW);
        }
-       
-       // step 1: set the latitude to be HIGH
-       digitalWrite(latitude.toInt(), HIGH);
-  
-       // step 2: set all the longitudes to be HIGH, so that everything's shut off
+   // step 2: set all the longitudes to be HIGH, so that everything's shut off
        for (int j=22; j<=52; j++) {
          digitalWrite(j, HIGH);
        }
+   
+   // first check if the latitude value is a number or not..
+   if (isdigit(latitude[0]) == 1) {  // the latitude will only be (2,10) so that's cool, it's only one char
+       // this is the case for a single LED
+       // step 1: set the latitude to be HIGH
+       digitalWrite(latitude.toInt(), HIGH);
   
-       // step 3: set the longitude which you need to be on, LOW. then you have a single LED on.
+       // step 2: set the longitude which you need to be on, LOW. then you have a single LED on.
        digitalWrite(longitude.toInt(), LOW);
        
      } else {
@@ -63,6 +63,7 @@ void lightHerUp(String latitude, String longitude) {
      switch(latitude[0]) {
        case 'h': {
          // Serial.println("hemisphere");
+         
          break;
        }
        case 'q': {
